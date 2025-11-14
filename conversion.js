6422c9c34e6837a48ebc5c50d856ba839074acca
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 <harshitbishtofficial2@gmail.com>
2025-11-14 17:12:00 +0530

    fixed the function temperature

---
+++ conversion.js
@@ -0,0 +1,24 @@
+function temperature(){
+    //to convert celsius to farenheit
+    //[cel * (9/5)+32]
+    var c = document.getElementById("celsius").value;
+    var f = [c * (9/5)+32];
+    document.getElementById("fahrenheit").value=f;
+}
+
+function weight(){
+    //to convert KGs to Pounds
+    //KG * 2.2
+    var kg =  document.getElementById("kilo").value;
+    var p = kg * 2.2;
+    document.getElementById("pounds").value = p;
+}
+
+function distance(){
+    //to convert Kms to miles
+    //kms * 0.62137
+    var km = document.getElementById("km").value;
+    var m = km + 2;
+    document.getElementById("miles").value = m;
+ }
+
